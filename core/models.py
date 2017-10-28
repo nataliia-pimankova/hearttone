f8e018b2980a997d6de25a2159b2c80d442a9250
Наталя Піманкова <natalya2487@gmail.com>
2017-10-29 02:14:49 +0300

    template for record list

---
+++ core/models.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class TimeStampedModel(models.Model):
+    """
+    An abstract base class model that provides self-
+    updating `` created`` and ``modified`` fields.
+    """
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
